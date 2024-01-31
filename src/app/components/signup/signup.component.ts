babdca51041c1ef050cd82c5ccbe5adb6ca7d78d
mohammed shafin <shafinms21@gmail.com>
2024-01-31 17:36:41 +0530

    header makes responsive and signup form created

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  selectedImg: any = null;
 
+  onFileSelected(event: any) {
+    this.selectedImg = event.target.files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = (readerEvent: any) => {
+      this.selectedImg = readerEvent.target.result;
+    };
+
+    reader.readAsDataURL(this.selectedImg);
+  }
 }
